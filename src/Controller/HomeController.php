d5439e67ba74a712b35b8cb0f077091a721ee703
Adeline CLERE <adeline@clere.me>
2018-03-27 17:37:18 +0200

    Publish users

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,19 +2,25 @@
 
 namespace App\Controller;
 
-use Symfony\Component\Routing\Annotation\Route;
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\Routing\Annotation\Route;
 
 class HomeController extends Controller
 {
     /**
      * @Route("/home", name="home")
      */
-    public function index() // à l'url home, cette fct° sera exécutée :
+    public function index(UserRepository $userRepo)  // à l'url home, cette fct° sera exécutée :
     {   // ns on va faire en sorte que notre appli return du HTML
         
-        $message ='Bonjour à tous'; // je vais transmettre msg a mon template
-        return $this->render("home.html.twig", ['msg' => $message]); 
+        $userList = $userRepo->findAll(); 
+        // $userRepo est passé automtqt en paramètre par Sfy
+        // -> injection de dépendances. On a dc pas à l'instancier ns-même.
+        // $userRepo effectuera ici un SELECT * FROM user ...
+        
+        // Dc a récup list user ac findAll et on transfère à notre vue ac render :
+        return $this->render("home.html.twig", ['users' => $userList]); 
         // on appelle le home.html.twig : on ajoute du contenu à Home + )
         // Dessine-moi ce template (home.html.twig) ac la variable msg qui vaudra message
     }       
